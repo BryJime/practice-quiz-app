883c70648ea5bc03ece90cde4b8e62c7e6d065cf
BryJime <bry.jimenex@gmail.com>
2026-01-11 21:11:14 -0800

    Inital question rendering logic

--- practice-project-quiz-app/src/components/QuizPage.jsx
+++ practice-project-quiz-app/src/components/QuizPage.jsx
@@ -8,20 +8,27 @@ function QuizPage() {
   const { state } = useLocation();
   const { category } = state;
 
-  const [questions, setQuestions] = useState([]);
+  const [ quizQuestion, setQuizQuestion ] = useState(QuizesData.filter(quiz => quiz.category === category)[0].question);
+  const [ QuestIndex, setQuestIndex ] = useState();
+  const [ questionOptions, setQuestionOptions ] = useState([]);
+
+  let quizQuestions = QuizesData.filter(quiz => quiz.category === category);
 
 
+  console.log(quizQuestions); 
 
-  let quizQuestions = QuizesData.map(quiz => {
-    quiz.category === category ? setQuestions(quiz.questions) : null
-  });
-
-  
-
-  const findCategoryQuestions = () => {
-
+  const nextQuestion = () => {
+    setQuizQuestion(quizQuestions[QuestIndex].question);
+    setQuestIndex(QuestIndex + 1);
   }
 
+  const previousQuestion = () => {
+    setQuestIndex(QuestIndex - 1);
+    setQuizQuestion(quizQuestions[QuestIndex].question);
+  }
+
+
+  console.log("Quiz Question: " + quizQuestion);
 
   return (
     <>
@@ -35,8 +42,8 @@ function QuizPage() {
 
           <div className="quiz-question-header">
             <div className="quiz-question-text-container">
-              <h1 className="quiz-question-number"> 1/10 </h1>
-              <h1 className="quiz-question-text"> Question Text Here </h1>
+              <h1 className="quiz-question-number"> {QuestIndex}/10 </h1>
+              <h1 className="quiz-question-text"> { quizQuestion } </h1>
             </div>
           </div>
 
@@ -53,9 +60,20 @@ function QuizPage() {
 
           <br />
 
+          <div className="quiz-navigation-buttons-container">
+            <div className="submit-button-container">
+              <button className="submit-answer-button"> Submit </button>
+            </div>
 
-          <div className="submit-button-container">
-            <button className="submit-answer-button"> Submit </button>
+            <div className="navigation-buttons-container">
+              <div className="previous-button-container">
+                <button className="previous-question-button" onClick={() => previousQuestion()}> Previous </button>
+                
+              </div>
+              <div className="next-button-container">
+                <button className="next-question-button" onClick={() => nextQuestion()}> Next </button>
+              </div>
+            </div>
           </div>
 
           <section className="quiz-answer-feedback-container">
